39835fefa7188c6aa29894954e61dffe6cad9e80
mosesbabu <mosesarara@gmail.com>
2025-06-15 15:44:25 +0300

    rename project to teamhub

--- src/page/auth/Sign-in.tsx
+++ src/page/auth/Sign-in.tsx
@@ -80,7 +80,7 @@ const SignIn = () => {
           className="flex items-center gap-2 self-center font-medium"
         >
           <Logo />
-          Team Sync.
+          Team Hub.
         </Link>
         <div className="flex flex-col gap-6">
           <Card>
